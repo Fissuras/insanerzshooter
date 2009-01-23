73962dba110bd4fbf57312129c5394ce9d13b2e5
mfdavid <mfdavid@ae054674-e31f-11dd-8fb1-9960f2a117f8>
2009-01-23 16:21:07 +0000

    Arrumando algumas coisas para compilar no Linux

--- createDebianPkg.sh
+++ createDebianPkg.sh
@@ -3,12 +3,12 @@
 rm -rf /tmp/insanerzshooter
 mkdir -p /tmp/insanerzshooter/DEBIAN
 mkdir -p /tmp/insanerzshooter/usr/games
-mkdir -p /tmp/insanerzshooter/usr/share/insanerzshooter/res
+mkdir -p /tmp/insanerzshooter/usr/share/insanerzshooter/
 mkdir -p /tmp/insanerzshooter/usr/share/applications
-cp res/* -r /tmp/insanerzshooter/usr/share/insanerzshooter/res
-cp hiscore.dat /tmp/insanerzshooter/usr/share/insanerzshooter/res
+cp res/* -r /tmp/insanerzshooter/usr/share/insanerzshooter/
+cp hiscore.dat /tmp/insanerzshooter/usr/share/insanerzshooter/
 cp bin/Release/InsanerzShooter /tmp/insanerzshooter/usr/games/insanerzshooter
-cp FreeSans_bold.ttf /tmp/insanerzshooter/usr/share/insanerzshooter/res 
+cp FreeSans_bold.ttf /tmp/insanerzshooter/usr/share/insanerzshooter/ 
 cp control /tmp/insanerzshooter/DEBIAN/
 cp insanerzshooter.desktop /tmp/insanerzshooter/usr/share/applications
 dpkg-deb -b /tmp/insanerzshooter/ .
